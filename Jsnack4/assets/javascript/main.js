06549e456154f2c302b3e345ea5c4f24a0bdbf28
matteopoliti <matteo.politi17@gmail.com>
2024-01-14 10:12:16 +0100

    feat(Jsnack4): added javascript function

---
+++ Jsnack4/assets/javascript/main.js
@@ -0,0 +1,11 @@
+/*Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.*/
+
+let numero = +prompt("Inserisci un numero")
+
+
+if(numero % 2 == 0){
+    document.writeln(numero)
+}else{
+    document.writeln(numero = numero + 1)
+}
+
